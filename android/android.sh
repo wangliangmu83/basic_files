45f9bc608f54fc68158f2bfd2f2a4f325dadf5be
kakaprince <kakaprince@163.com>
2024-08-26 07:26:43 +0800

    更新 android.sh

--- android/android.sh
+++ android/android.sh
@@ -14,6 +14,7 @@ update_upgrade_packages() {
 install_necessary_packages() {
     log "安装必要的软件包..."
     pkg install vim openssh proot-distro -y
+    pkg install expect
 }
 
 generate_ssh_host_keys() {
@@ -175,13 +176,23 @@ restart_ssh_service
 
 update_upgrade_packages
 
-# 复制密钥文件到ubuntu用户
-mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/
+# 复制密钥文件到 ubuntu 用户
+mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.ssh
 cp /.ssh/authorized_keys /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.ssh/
+if [ $? -eq 0 ]; then
+    echo "/.ssh/authorized_keys 复制到 /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.ssh/ 成功"
+else
+    echo "复制到 /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.ssh/ 失败"
+fi
 
-# 复制密钥文件到gitsync用户
-mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/gitsync/
-cp /data/data/com.termux/files/home/.ssh/authorized_keys /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/gitsync/.ssh
+# 复制密钥文件到 gitsync 用户
+mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/gitsync/.ssh
+cp /data/data/com.termux/files/home/.ssh/authorized_keys /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/gitsync/.ssh/
+if [ $? -eq 0 ]; then
+    echo "/data/data/com.termux/files/home/.ssh/authorized_keys 复制到 /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/gitsync/.ssh/ 成功"
+else
+    echo "复制到 /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/gitsync/.ssh/ 失败"
+fi
 
 # 启动 proot-distro 并登录到 Ubuntu
 proot-distro login ubuntu 
@@ -192,19 +203,11 @@ apt upgrade -y
 # 安装expect
 apt install -y expect
 
-# 设定一个默认密码
-PASSWORD="19831102Wq"
-
 #下载git.sh.enc
 curl https://raw.githubusercontent.com/wangliangmu83/basic_files/main/android/git.sh.enc >git.sh.enc
 
-# 使用 expect 自动输入密码解密文件
-expect <<EOF
-spawn openssl aes-256-cbc -d -pbkdf2 -in git.sh.enc -out git.sh
-expect "enter AES-256-CBC decryption password:"
-send "$PASSWORD\r"
-expect eof
-EOF
+# 输入密码解密文件
+openssl aes-256-cbc -d -pbkdf2 -in git.sh.enc -out git.sh
 
 #授权git.sh
 chmod +x git.sh 
