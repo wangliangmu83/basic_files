64fb7c3e64f54902f34c24ac706d8bd986146cc9
kakaprince <kakaprince@163.com>
2024-08-27 00:14:52 +0800

    将安装文件分拆成两部分

--- newubuntu.sh
+++ newubuntu.sh
@@ -73,29 +73,5 @@ if ! grep -Fxq "$(cat "$PUBLIC_KEY")" "$AUTHORIZED_KEYS"; then
     cat "$PUBLIC_KEY" | sudo tee -a "$AUTHORIZED_KEYS" > /dev/null
 fi
 
-log "配置sshd_config..."
-
-# 定义sshd_config文件路径
-SSHD_CONFIG_FILE="/etc/ssh/sshd_config"
-
-# 定义要添加到sshd_config文件中的配置行
-config_lines=(
-    "PermitRootLogin yes"
-    "ListenAddress 0.0.0.0"
-    "PubkeyAuthentication yes"
-    "AuthorizedKeysFile %h/.ssh/authorized_keys"
-    "PasswordAuthentication no"  # 添加这一行来禁用密码认证
-)
-
-# 遍历配置行并添加到sshd_config文件中
-for line in "${config_lines[@]}"; do
-    if ! grep -q "^${line}$" "$SSHD_CONFIG_FILE"; then
-        echo "${line}" | sudo tee -a "$SSHD_CONFIG_FILE" > /dev/null
-    fi
-done
-
-# 开机启动ssh
-sudo systemctl enable ssh
-
 # 重启SSH服务
 sudo systemctl restart ssh
