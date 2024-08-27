2cfdcb0d38c4fa42643ee5af147b63987f1b1bfb
kakaprince <kakaprince@163.com>
2024-08-27 18:39:58 +0800

    更新 mail.sh

--- mail.sh
+++ mail.sh
@@ -4,12 +4,12 @@
 sudo apt update && sudo apt upgrade -y
 
 # 安装必要的软件包
-sudo apt install -y postfix dovecot-core dovecot-imapd dovecot-pop3d openssl
+sudo apt install -y postfix dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd openssl
 
 # 配置Postfix
 sudo tee /etc/postfix/main.cf > /dev/null <<EOL
-myhostname = mail.yourdomain.com
-mydomain = yourdomain.com
+myhostname = mail.bestwood.asia
+mydomain = bestwood.asia
 myorigin = /etc/mailname
 inet_interfaces = all
 mydestination = \$myhostname, localhost.\$mydomain, localhost, \$mydomain
@@ -32,8 +32,18 @@ sudo tee /etc/dovecot/conf.d/10-mail.conf > /dev/null <<EOL
 mail_location = maildir:~/Maildir
 EOL
 
+sudo tee /etc/dovecot/conf.d/10-master.conf > /dev/null <<EOL
+service lmtp {
+  unix_listener /var/spool/postfix/private/dovecot-lmtp {
+    mode = 0600
+    user = postfix
+    group = postfix
+  }
+}
+EOL
+
 # 生成SSL证书
-sudo openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/mailserver.pem -keyout /etc/ssl/private/mailserver.key -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=mail.yourdomain.com"
+sudo openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/mailserver.pem -keyout /etc/ssl/private/mailserver.key -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=mail.bestwood.asia"
 
 # 重启服务
 sudo systemctl restart postfix
