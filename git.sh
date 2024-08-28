f2f7e6c5a1dca25b268441192f36cc9b86a5d70a
kakaprince <kakaprince@163.com>
2024-08-28 18:34:30 +0800

    更新 git.sh

--- git.sh
+++ git.sh
@@ -7,9 +7,6 @@ log() {
 # 设定一个默认密码
 PASSWORD="19831102Wq"
 
-apt update && apt upgrade -y         
-apt install -y expect
-    
 # 设置用户密码的函数
 set_user_password() {
     
@@ -30,10 +27,6 @@ EOF
         log "密码设置失败，请重新尝试。"
     fi
 }
-
-# 首先修改root的密码
-set_user_password root
-
 # 定义函数
 update_upgrade_packages() {
     log "更新并升级现有的包..."
@@ -46,10 +39,11 @@ install_necessary_packages() {
     apt install -y openssl
 
 }
-
 update_upgrade_packages
 install_necessary_packages
 
+# 首先修改root的密码
+set_user_password root
 
 # # 添加gitsync用户
 log "添加gitsync用户..."
@@ -61,35 +55,20 @@ useradd -m -s /usr/bin/git-shell -c "Git Sync User" -k /nonexistent gitsync
 # -c "Git Sync User": 设置用户的描述信息
 # -k /nonexistent: 设置用户的默认系统配置文件，如果指定文件不存在则不使用
 
-# 检查/home/gitsync目录是否已存在
-if [ ! -d "/home/gitsync" ]; then
-    # 如果不存在，则创建目录
-    mkdir -p /home/gitsync
-    mkdir -p /home/gitsync/.ssh
-fi
-
-# 确保设置目录的所有权和权限
-chown gitsync:gitsync /home/gitsync  # 设置所有权为gitsync用户
-chmod 755 /home/gitsync  # 设置权限
-
-# 检查用户添加是否成功
-if [ $? -eq 0 ]; then
-    log "gitsync用户添加成功!"
-else
-    log "gitsync用户添加失败，请重新尝试。"
-fi
-
 # 设置gitsync用户的密码
 set_user_password gitsync
 
-# gitsync密钥文件授权
-chmod 700 /home/gitsync/.ssh
-
+# /home/gitsync创建目录
+mkdir -p /home/gitsync/.ssh
 cp /root/.ssh/authorized_keys /home/gitsync/.ssh
-chmod 600 /home/gitsync/.ssh/authorized_keys
-chown -R gitsync:gitsync /home/gitsync/.ssh
-chown -R gitsync:gitsync /home/gitsync/.ssh/authorized_keys
 
+# 确保设置目录的所有权和权限
+chown -R gitsync:gitsync /home/gitsync  # 设置所有权为gitsync用户
+
+# gitsync密钥文件授权
+chmod 755 /home/gitsync 
+chmod 700 /home/gitsync/.ssh
+chmod 600 /home/gitsync/.ssh/authorized_keys
 
 # 切换到gitsync用户
 su - gitsync <<-'EOF'
