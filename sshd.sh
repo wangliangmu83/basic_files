b26f66d70a5999c7700fa1f762e5f4b931462989
kakaprince <kakaprince@163.com>
2024-08-28 22:01:58 +0800

    更新 sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -11,23 +11,23 @@ touch "$AUTHORIZED_KEYS" && chmod 600 "$AUTHORIZED_KEYS"
 
 # 如果私钥或公钥文件不存在，则生成新的密钥对
 if [ ! -f "$PRIVATE_KEY" ] || [ ! -f "$PUBLIC_KEY" ]; then
-  read -p "No SSH keys found. Generate new keys? (y/n): " confirm
-  if [ "$confirm" = "y" ]; then
-    echo "Generating SSH keys without a passphrase."
-    ssh-keygen -t rsa -b 4096 -C "king_rush@gmail.com" -f "$PRIVATE_KEY" -N ""
-    chmod 600 "$PRIVATE_KEY"
-    chmod 644 "$PUBLIC_KEY"
-  else
-    echo "SSH key generation cancelled."
-  fi
+    read -p "No SSH keys found. Generate new keys? (y/n): " confirm
+    if [ "$confirm" = "y" ]; then
+        echo "Generating SSH keys without a passphrase."
+        ssh-keygen -t rsa -b 4096 -C "king_rush@gmail.com" -f "$PRIVATE_KEY" -N ""
+        chmod 600 "$PRIVATE_KEY"
+        chmod 644 "$PUBLIC_KEY"
+    else
+        echo "SSH key generation cancelled."
+    fi
 else
-  echo "SSH keys already exist."
+    echo "SSH keys already exist."
 fi
 
 # 如果公钥不在authorized_keys文件中，则追加公钥
 if ! grep -Fxq "$(cat "$PUBLIC_KEY")" "$AUTHORIZED_KEYS"; then
-  cat "$PUBLIC_KEY" | tee -a "$AUTHORIZED_KEYS" >/dev/null
-  # 不删除公钥文件
+    cat "$PUBLIC_KEY" | tee -a "$AUTHORIZED_KEYS" >/dev/null
+    # 不删除公钥文件
 fi
 
 # 复制 authorized_keys 到 gitsync 用户
